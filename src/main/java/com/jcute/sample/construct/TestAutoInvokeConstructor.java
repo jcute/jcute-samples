47713a7112f947ab47e1126d9a6c8fa9b4c4d7ae
tb_bin@163.com <passw0rd>
2018-04-14 10:10:22 +0800

    添加构造函数选举测试用例

--- src/main/java/com/jcute/sample/construct/TestAutoInvokeConstructor.java
+++ src/main/java/com/jcute/sample/construct/TestAutoInvokeConstructor.java
@@ -1,5 +1,48 @@
 package com.jcute.sample.construct;
 
-public class TestAutoInvokeConstructor{
+import com.jcute.core.annotation.Autowired;
+import com.jcute.core.annotation.ComponentScan;
+import com.jcute.core.annotation.Configuration;
+import com.jcute.core.annotation.Initial;
+import com.jcute.core.boot.JCuteApplication;
+import com.jcute.sample.service.impl.AdministratorUserInfoService;
+import com.jcute.sample.service.impl.StandardUserInfoService;
 
+/**
+ * 测试自定义构造函数实例化
+ * @author tangbin
+ *
+ */
+@Configuration
+@ComponentScan("com.jcute.sample.service.impl")
+public class TestAutoInvokeConstructor{
+	
+	public static void main(String[] args){
+		JCuteApplication application = JCuteApplication.run(TestAutoInvokeConstructor.class);
+		TestAutoInvokeConstructor con = application.getApplicationContext().getBean(TestAutoInvokeConstructor.class);
+		System.out.println(con);
+	}
+	
+	//构造函数选举优先级
+	//最低
+	public TestAutoInvokeConstructor(String name){
+		System.out.println("constructor -> " + name);
+	}
+	
+	//参数含有@Autowited其次
+	public TestAutoInvokeConstructor(@Autowired StandardUserInfoService service){
+		System.out.println(service.getUserName());
+	}
+	
+	//构造函数上还有@Initial注解最高
+	@Initial
+	public TestAutoInvokeConstructor(@Autowired AdministratorUserInfoService service){
+		System.out.println(service);
+	}
+	
+	@Initial
+	public void initial(){
+		System.out.println("initial");
+	}
+	
 }
